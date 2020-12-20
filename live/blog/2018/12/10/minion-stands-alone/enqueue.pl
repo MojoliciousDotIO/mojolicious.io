8681cb7287ee6999e32a979c5f634640f5cad6c5
Joel Berger <joel.a.berger@gmail.com>
2020-12-20 14:01:38 -0600

    attempt to deploy to gh-pages via github actions

--- live/blog/2018/12/10/minion-stands-alone/enqueue.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env perl
-use Minion;
-my $minion = Minion->new(
-    SQLite => 'sqlite:minion.db', # The same database as the worker
-);
-$minion->enqueue(
-    check_url => ['http://mojolicious.org'],
-);
